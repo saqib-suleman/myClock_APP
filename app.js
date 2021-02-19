10df1599962420cc22be85190c1e208a6e2bcde8
Saqib Suleman <saqibs3291@gmail.com>
2021-02-19 13:22:09 +0500

    add script logic

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const dialLines = document.querySelector('.dialLines');
-const clock = document.querySelector('.clock');
+setInterval(setClock, 1000);
 
-for (let i = 1; i < 12; i++) {
-    clock.innerHTML = + "<div class="dialLines"></div>";
-    dialLines[i].style.tranform = "rotate(" + 30 * i + "deg)";
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
